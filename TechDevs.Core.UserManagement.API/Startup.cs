e25641316869f2c63847495f8fe5527cca9fe462
Steve Kent <stevekent55@me.com>
2018-02-10 11:30:53 +0000

    * temp.sql: Removed docker support
    * TechDevs.sln:
    * docker-compose.yml:
    * docker-compose.dcproj:
    * docker-compose.ci.build.yml:
    * docker-compose.override.yml:
    * Dockerfile:
    * Program.cs:
    * Startup.cs:
    * .dockerignore:
    * appsettings.json:
    * appsettings.Development.json:

---
+++ TechDevs.Core.UserManagement.API/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using TechDevs.Core.UserManagement.Interfaces;
+using TechDevs.Core.UserManagement.Repositories;
+using TechDevs.Core.UserManagement.Services;
+
+namespace TechDevs.Core.UserManagement.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IUserProfileService, UserProfileService>();
+            services.AddTransient<IUserRegistrationService, UserRegistrationService>();
+            services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "User Profile API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "User Profile API v1");
+            });
+        }
+    }
+}
