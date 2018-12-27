4e3caea78730c420d136e0f1e574faa68828004e
Steve <stevekent55@gmail.com>
2018-12-27 00:56:03 +0000

    Updated typo in accounts

--- WebServices/TechDevs.Accounts.WebService/Configuration/MongoDBConfig.cs
+++ WebServices/TechDevs.Accounts.WebService/Configuration/MongoDBConfig.cs
@@ -18,67 +18,11 @@ namespace TechDevs.Accounts.WebService
                 c.SetDiscriminatorIsRequired(true);
             });
 
-            // BsonClassMap.RegisterClassMap<CustomerData>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<CustomerVehicle>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<MotResult>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<MotComment>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-
             BsonClassMap.RegisterClassMap<Employee>(c =>
             {
                 c.SetDiscriminator("Employee");
                 c.SetDiscriminatorIsRequired(true);
             });
-
-            // BsonClassMap.RegisterClassMap<EmployeeData>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-
-            // BsonClassMap.RegisterClassMap<Client>(c =>
-            // {
-            //     c.SetDiscriminator("Client");
-            //     c.SetDiscriminatorIsRequired(true);
-            // });
-
-            // BsonClassMap.RegisterClassMap<EmployeeProfile>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<CustomerProfile>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<ClientTheme>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-            // BsonClassMap.RegisterClassMap<CSSParameter>(c =>
-            // {
-            //     c.AutoMap();
-            //     c.SetIgnoreExtraElements(true);
-            // });
-
-
         }
     }
 }
