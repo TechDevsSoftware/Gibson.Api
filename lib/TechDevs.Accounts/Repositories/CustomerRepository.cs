16444935f604eff9d056b60cd76eb28257f75fb9
Steve <stevekent55@gmail.com>
2019-01-13 14:06:44 +0000

    Fixed new registration bug

--- lib/TechDevs.Accounts/Repositories/CustomerRepository.cs
+++ lib/TechDevs.Accounts/Repositories/CustomerRepository.cs
@@ -10,21 +10,5 @@ namespace TechDevs.Accounts.Repositories
         public CustomerRepository(IOptions<MongoDbSettings> dbSettings, IStringNormaliser normaliser) : base(dbSettings, normaliser)
         {
         }
-
-        public override async Task<Customer> FindById(string id, string clientId)
-        {
-            var result = await base.FindById(id, clientId);
-            if(result?.CustomerData?.MarketingPreferences == null) result.CustomerData.MarketingPreferences = new MarketingNotificationPreferences();
-            if(result?.CustomerData?.NotificationPreferences == null) result.CustomerData.NotificationPreferences = new CustomerNotificationPreferences();
-            return result;
-        }
-
-        public override async Task<Customer> FindByEmail(string email, string clientId)
-        {
-            var result = await base.FindByEmail(email, clientId);
-            if(result?.CustomerData?.MarketingPreferences == null) result.CustomerData.MarketingPreferences = new MarketingNotificationPreferences();
-            if(result?.CustomerData?.NotificationPreferences == null) result.CustomerData.NotificationPreferences = new CustomerNotificationPreferences();
-            return result;
-        }
     }
 }
